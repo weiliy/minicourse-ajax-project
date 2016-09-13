1711a6c65bcdd6ab5a12281879de0e7dfc8bf218
weiliy <yi@weiliy.net>
2016-09-13 22:44:32 +0800

    feat: ErrorHandling with NY Times

--- js/script.js
+++ js/script.js
@@ -32,6 +32,8 @@ function loadData() {
                 '<p>' + article.snippet + '</p>' +
             '</li>');
         };
+    }).error(function(e){
+        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
     });
 
     return false;
